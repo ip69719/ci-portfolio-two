8bafb1e4737ac5043987044df951016652bd8b93
ip69719 <ipeters@mail.com>
2022-10-30 01:35:14 +0000

    Create sample questions for the game

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Create questions for the game
+
+let questions = [
+    {
+        question: 'assets/images/peugeot-logo.png',
+        answers: [
+            {text: 'mercedes', correct: false},
+            {text: 'peugeot', correct: true},
+            {text: 'tesla', correct: false}
+        ]
+    },
+    {
+        question: 'assets/images/wiesmann-logo.png',
+        answers: [
+            {text: 'seat', correct: false},
+            {text: 'honda', correct: false},
+            {text: 'wiesmann', correct: true}
+        ]
+    }
+]
