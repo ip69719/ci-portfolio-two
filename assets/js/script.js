19e492bc878f2f3cf117bc9833ebf2ebef4c4d27
ip69719 <ipeters@mail.com>
2022-10-30 14:32:13 +0000

    Change structure of quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,18 @@
 
 let questions = [
     {
-        question: 'assets/images/peugeot-logo.png',
-        answers: [
-            {text: 'mercedes', correct: false},
-            {text: 'peugeot', correct: true},
-            {text: 'tesla', correct: false}
-        ]
+        questionImg: 'assets/images/peugeot-logo.png',
+        answerOptionA: 'mercedes',
+        answerOptionB: 'peugeot',
+        answerOptionC: 'tesla',
+        correct: 'answerOptionB'
     },
     {
-        question: 'assets/images/wiesmann-logo.png',
-        answers: [
-            {text: 'seat', correct: false},
-            {text: 'honda', correct: false},
-            {text: 'wiesmann', correct: true}
-        ]
+        questionImg: 'assets/images/wiesmann-logo.png',
+        answerOptionA: 'seat',
+        answerOptionB: 'honda',
+        answerOptionC: 'tesla',
+        correct: 'wiesmann'
     }
 ]
 
