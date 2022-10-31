82874e02d8fe040defd2001ab479b05ed37f739c
ip69719 <ipeters@mail.com>
2022-10-31 19:44:23 +0000

    Move questions to separate JS file

---
+++ assets/js/questions.js
@@ -0,0 +1,25 @@
+// Create questions for the game
+
+let questions = [
+    {
+        questionImg: 'assets/images/peugeot-logo.png',
+        answerOptionA: 'mercedes',
+        answerOptionB: 'peugeot',
+        answerOptionC: 'tesla',
+        correct: 'answerOptionB'
+    },
+    {
+        questionImg: 'assets/images/wiesmann-logo.png',
+        answerOptionA: 'seat',
+        answerOptionB: 'honda',
+        answerOptionC: 'wiesmann',
+        correct: 'answerOptionC'
+    },
+    {
+        questionImg: 'assets/images/vauxhall-logo.png',
+        answerOptionA: 'vauxhall',
+        answerOptionB: 'subaru',
+        answerOptionC: 'audi',
+        correct: 'answerOptionA'
+    }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,3 @@
-// Create questions for the game
-
-let questions = [
-    {
-        questionImg: 'assets/images/peugeot-logo.png',
-        answerOptionA: 'mercedes',
-        answerOptionB: 'peugeot',
-        answerOptionC: 'tesla',
-        correct: 'answerOptionB'
-    },
-    {
-        questionImg: 'assets/images/wiesmann-logo.png',
-        answerOptionA: 'seat',
-        answerOptionB: 'honda',
-        answerOptionC: 'wiesmann',
-        correct: 'answerOptionC'
-    }
-    {
-        questionImg: 'assets/images/wiesmann-logo.png',
-        answerOptionA: 'vauxhall-logo',
-        answerOptionB: 'subaru',
-        answerOptionC: 'audi',
-        correct: 'answerOptionC'
-    }
-]
-
-
 //Declare variables
 
 //to keep track of current question
