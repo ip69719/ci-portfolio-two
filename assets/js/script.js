bec40cc27f485b3633aac0fd8e4e07e28d3f15da
ip69719 <ipeters@mail.com>
2022-11-08 22:13:52 +0000

    Add JS code for dark and light theme switcher

--- assets/js/script.js
+++ assets/js/script.js
@@ -204,3 +204,29 @@ function returnHome() {
     //reset score
     score = 0;
 }
+
+
+/* Learned how to create dark and light themes using CSS variables and JavaScript and adopted the below code from this
+(https://medium.com/@haxzie/dark-and-light-theme-switcher-using-css-variables-and-pure-javascript-zocada-dd0059d72fa2) tutorial */
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
