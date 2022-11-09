c6707f2011797eb0f32f66c869523eb0adf799cb
ip69719 <ipeters@mail.com>
2022-11-09 10:25:47 +0000

    Add questions for the quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,24 +1,144 @@
-// Create questions for the game
+/*jshint esversion: 6 */
+// Create questions for the quiz
 
 let questions = [
     {
         questionImg: 'assets/images/peugeot-logo.png',
         answerOptionA: 'mercedes',
         answerOptionB: 'peugeot',
-        answerOptionC: 'tesla',
+        answerOptionC: 'holden',
         correct: 'answerOptionB'
     },
     {
         questionImg: 'assets/images/wiesmann-logo.png',
         answerOptionA: 'seat',
-        answerOptionB: 'honda',
+        answerOptionB: 'toyota',
         answerOptionC: 'wiesmann',
         correct: 'answerOptionC'
     },
     {
         questionImg: 'assets/images/vauxhall-logo.png',
         answerOptionA: 'vauxhall',
-        answerOptionB: 'subaru',
+        answerOptionB: 'volkswagen',
+        answerOptionC: 'volvo',
+        correct: 'answerOptionA'
+    },
+    {
+        questionImg: 'assets/images/subaru-logo.png',
+        answerOptionA: 'subaru',
+        answerOptionB: 'lincoln',
+        answerOptionC: 'chevrolet',
+        correct: 'answerOptionA'
+    },
+    {
+        questionImg: 'assets/images/citroen-logo.png',
+        answerOptionA: 'seat',
+        answerOptionB: 'citroen',
+        answerOptionC: 'corvette',
+        correct: 'answerOptionB'
+    },
+    {
+        questionImg: 'assets/images/chevrolet-logo.png',
+        answerOptionA: 'chevrolet',
+        answerOptionB: 'porsche',
+        answerOptionC: 'smart',
+        correct: 'answerOptionA'
+    },
+    {
+        questionImg: 'assets/images/renault-logo.png',
+        answerOptionA: 'qoros',
+        answerOptionB: 'mazda',
+        answerOptionC: 'renault',
+        correct: 'answerOptionC'
+    },
+    {
+        questionImg: 'assets/images/infiniti-logo.png',
+        answerOptionA: 'opel',
+        answerOptionB: 'daewoo',
+        answerOptionC: 'infiniti',
+        correct: 'answerOptionC'
+    },
+    {
+        questionImg: 'assets/images/seat-logo.png',
+        answerOptionA: 'tesla',
+        answerOptionB: 'smart',
+        answerOptionC: 'seat',
+        correct: 'answerOptionC'
+    },
+    {
+        questionImg: 'assets/images/corvette-logo.png',
+        answerOptionA: 'corvette',
+        answerOptionB: 'pontiac',
+        answerOptionC: 'chrysler',
+        correct: 'answerOptionA'
+    },
+    {
+        questionImg: 'assets/images/daewoo-logo.png',
+        answerOptionA: 'honda',
+        answerOptionB: 'daewoo',
+        answerOptionC: 'hyundai',
+        correct: 'answerOptionB'
+    },
+    {
+        questionImg: 'assets/images/lexus-logo.png',
+        answerOptionA: 'lancia',
+        answerOptionB: 'lotus',
+        answerOptionC: 'lexus',
+        correct: 'answerOptionC'
+    },
+    {
+        questionImg: 'assets/images/koenigsegg-logo.png',
+        answerOptionA: 'acura',
+        answerOptionB: 'koenigsegg',
+        answerOptionC: 'abarth',
+        correct: 'answerOptionB'
+    },
+    {
+        questionImg: 'assets/images/qoros-logo.png',
+        answerOptionA: 'qoros',
+        answerOptionB: 'rover',
+        answerOptionC: 'mini',
+        correct: 'answerOptionA'
+    },
+    {
+        questionImg: 'assets/images/smart-logo.png',
+        answerOptionA: 'smart',
+        answerOptionB: 'audi',
+        answerOptionC: 'volvo',
+        correct: 'answerOptionA'
+    },
+    {
+        questionImg: 'assets/images/pontiac-logo.png',
+        answerOptionA: 'pagani',
+        answerOptionB: 'pontiac',
+        answerOptionC: 'tesla',
+        correct: 'answerOptionB'
+    },
+    {
+        questionImg: 'assets/images/bentley-logo.png',
+        answerOptionA: 'buick',
+        answerOptionB: 'bentley',
+        answerOptionC: 'bmw',
+        correct: 'answerOptionB'
+    },
+    {
+        questionImg: 'assets/images/mazda-logo.png',
+        answerOptionA: 'mazda',
+        answerOptionB: 'toyota',
+        answerOptionC: 'dacia',
+        correct: 'answerOptionA'
+    },
+    {
+        questionImg: 'assets/images/maybach-logo.png',
+        answerOptionA: 'buick',
+        answerOptionB: 'maybach',
+        answerOptionC: 'arash',
+        correct: 'answerOptionB'
+    },
+    {
+        questionImg: 'assets/images/mercedes-benz-logo.png',
+        answerOptionA: 'mercedes',
+        answerOptionB: 'opel',
         answerOptionC: 'audi',
         correct: 'answerOptionA'
     }
